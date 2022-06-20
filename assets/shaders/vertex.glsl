30c003c9b793c6f7443eefbf4b127e046069ae39
philipab <philip231001@googlemail.com>
2022-06-20 12:46:50 +0200

    Update vertex shader

--- assets/shaders/vertex.glsl
+++ assets/shaders/vertex.glsl
@@ -4,10 +4,12 @@ layout (location = 0) in vec3 pos;
 layout (location = 1) in vec3 colorRGB;
 
 uniform mat4 modelmatrix;
+uniform mat4 view_matrix;
+uniform mat4 projection_matrix;
 
 out vec3 colorVS;
 
 void main(){
     colorVS = colorRGB;
-    gl_Position = modelmatrix * vec4(pos.x, pos.y, pos.z, 1.0);
+    gl_Position = modelmatrix * view_matrix * projection_matrix * vec4(pos.x, pos.y, pos.z, 1.0);
 }
